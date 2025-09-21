7bb4c0f5710eb3ea3370a3429c51a44d9e10600f
koukimori <jieihal090310@gmail.com>
2025-09-21 22:32:26 +0900

    Update GitHub Pages with fixed Instagram logo and latest changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3142412445"
+    serviceWorkerVersion: "1831934475"
   }
 });
